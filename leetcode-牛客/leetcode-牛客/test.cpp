c9fd7497b687778c8bedc92cd50215714a32be09
Aircraft-King <1069865680@qq.com>
2019-12-05 18:19:52 +0800

    leetcode-tree

--- leetcode-牛客/leetcode-牛客/test.cpp
+++ leetcode-牛客/leetcode-牛客/test.cpp
@@ -7,6 +7,36 @@
 #include <algorithm> //sort
 using namespace std;
 
+//https://leetcode-cn.com/problems/lowest-common-ancestor-of-a-binary-tree/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+	TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+		if (root == NULL || root == p || root == q) {
+			return root;
+		}
+		TreeNode* left = lowestCommonAncestor(root->left, p, q);
+		TreeNode* right = lowestCommonAncestor(root->right, p, q);
+		if (left&&right) {
+			return root;
+		}
+		return left ? left : right;
+	}
+};
+
+
+
+
+
 //https://leetcode-cn.com/problems/valid-anagram/
 class Solution {
 public:
