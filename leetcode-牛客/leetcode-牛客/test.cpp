4dc83d9f8632b22c15b8d035e362c6e49da9fe4f
Aircraft-King <1069865680@qq.com>
2019-12-06 21:20:24 +0800

    leetcode

--- leetcode-牛客/leetcode-牛客/test.cpp
+++ leetcode-牛客/leetcode-牛客/test.cpp
@@ -7,6 +7,35 @@
 #include <algorithm> //sort
 using namespace std;
 
+//https://leetcode-cn.com/problems/lowest-common-ancestor-of-a-binary-search-tree/submissions/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+	TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+		if (root == NULL || root == p || root == q
+			|| (root != NULL && root->val > p->val&&root->val < q->val)
+			|| (root != NULL && root->val<p->val&&root->val>q->val)) {
+			return root;
+		}
+
+		if (p->val < root->val&&q->val < root->val&&root != NULL) {
+			return lowestCommonAncestor(root->left, p, q);
+		}
+		if (p->val > root->val&&q->val > root->val&&root != NULL) {
+			return lowestCommonAncestor(root->right, p, q);
+		}
+		return root;
+	}
+};
+
 //https://leetcode-cn.com/problems/lowest-common-ancestor-of-a-binary-tree/
 
 /**
