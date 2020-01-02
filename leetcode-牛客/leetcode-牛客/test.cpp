d51a7a83cc69d75ee9898a18db16dabcc9f1c61a
Aircraft-King <1069865680@qq.com>
2020-01-02 13:44:52 +0800

    leetcode-二进制求和

--- leetcode-牛客/leetcode-牛客/test.cpp
+++ leetcode-牛客/leetcode-牛客/test.cpp
@@ -3,32 +3,107 @@
 #include<vector>
 #include<list>
 #include<stack>
+#include<assert.h>
 #include<deque>
 #include <algorithm> //sort
 using namespace std;
 
-//https://leetcode-cn.com/problems/plus-one/submissions/
+//https://leetcode-cn.com/problems/add-binary/submissions/
+
 class Solution {
 public:
-	vector<int> plusOne(vector<int>& digits) {
-		int i = digits.size() - 1;
-		for (; i >= 0; --i) {
-			if (digits[i] == 9) {
-				digits[i] = 0;
-				if (i - 1 < 0) {
-					digits[i] = 1;
-					digits.push_back(0);
-				}
-			}
-			else {
-				digits[i]++;
-				break;
-			}
+	string addBinary(string a, string b) {
 
+		int a_len = a.size();
+		int b_len = b.size();
+		while (a_len < b_len) {
+			a = '0' + a;
+			a_len++;
 		}
-		return digits;
+		while (b_len < a_len) {
+			b = '0' + b;
+			b_len++;
+		}
+		for (int i = a_len - 1; i > 0; --i) {
+			a[i] = a[i] - '0' + b[i];
+			if (a[i] >= '2') {
+				a[i] = (a[i] - '0') % 2 + '0';
+				a[i - 1] = a[i - 1] + 1;
+			}
+		}
+		a[0] = a[0] - '0' + b[0];
+		if (a[0] >= '2') {
+			a[0] = (a[0] - '0') % 2 + '0';
+			a = '1' + a;
+		}
+		return a;
 	}
 };
+
+string stringToString(string input) {
+	assert(input.length() >= 2);
+	string result;
+	for (int i = 1; i < input.length() - 1; i++) {
+		char currentChar = input[i];
+		if (input[i] == '\\') {
+			char nextChar = input[i + 1];
+			switch (nextChar) {
+			case '\"': result.push_back('\"'); break;
+			case '/': result.push_back('/'); break;
+			case '\\': result.push_back('\\'); break;
+			case 'b': result.push_back('\b'); break;
+			case 'f': result.push_back('\f'); break;
+			case 'r': result.push_back('\r'); break;
+			case 'n': result.push_back('\n'); break;
+			case 't': result.push_back('\t'); break;
+			default: break;
+			}
+			i++;
+		}
+		else {
+			result.push_back(currentChar);
+		}
+	}
+	return result;
+}
+
+int main() {
+	string line;
+	while (getline(cin, line)) {
+		string a = stringToString(line);
+		getline(cin, line);
+		string b = stringToString(line);
+
+		string ret = Solution().addBinary(a, b);
+
+		string out = (ret);
+		cout << out << endl;
+	}
+	return 0;
+}
+
+//https://leetcode-cn.com/problems/plus-one/submissions/
+//class Solution {
+//public:
+//	vector<int> plusOne(vector<int>& digits) {
+//		int i = digits.size() - 1;
+//		for (; i >= 0; --i) {
+//			if (digits[i] == 9) {
+//				digits[i] = 0;
+//				if (i - 1 < 0) {
+//					digits[i] = 1;
+//					digits.push_back(0);
+//				}
+//			}
+//			else {
+//				digits[i]++;
+//				break;
+//			}
+//
+//		}
+//		return digits;
+//	}
+//};
 
 //https://leetcode-cn.com/problems/remove-element/submissions/
 
