6da3a1fc91d472ac659f0ae02ff6477aefd0037a
Aircraft-King <1069865680@qq.com>
2020-01-05 16:09:38 +0800

    leetcode-合并两个有序数组

--- leetcode-牛客/leetcode-牛客/test.cpp
+++ leetcode-牛客/leetcode-牛客/test.cpp
@@ -8,27 +8,106 @@
 #include <algorithm> //sort
 using namespace std;
 
-//https://leetcode-cn.com/problems/majority-element/submissions/
+//https://leetcode-cn.com/problems/merge-sorted-array/
 
 class Solution {
 public:
-	int majorityElement(vector<int>& nums) {
-		int target = nums[0];
-		int count = 1;
-		for (int i = 1; i < nums.size(); i++)
-			if (nums[i] != target)
-			{
-				count--;
-				if (count == 0)
-				{
-					target = nums[i];
-					count = 1;
-				}
-			}
-			else count++;
-		return target;
+	void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+		for (int i = m, j = 0; i < m + n, j < n; ++i, ++j) {
+			nums1[i] = nums2[j];
+		}
+		sort(nums1.begin(), nums1.end());
 	}
 };
+
+void trimLeftTrailingSpaces(string &input) {
+	input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+		return !isspace(ch);
+	}));
+}
+
+void trimRightTrailingSpaces(string &input) {
+	input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+		return !isspace(ch);
+	}).base(), input.end());
+}
+
+vector<int> stringToIntegerVector(string input) {
+	vector<int> output;
+	trimLeftTrailingSpaces(input);
+	trimRightTrailingSpaces(input);
+	input = input.substr(1, input.length() - 2);
+	stringstream ss;
+	ss.str(input);
+	string item;
+	char delim = ',';
+	while (getline(ss, item, delim)) {
+		output.push_back(stoi(item));
+	}
+	return output;
+}
+
+int stringToInteger(string input) {
+	return stoi(input);
+}
+
+string integerVectorToString(vector<int> list, int length = -1) {
+	if (length == -1) {
+		length = list.size();
+	}
+
+	if (length == 0) {
+		return "[]";
+	}
+
+	string result;
+	for (int index = 0; index < length; index++) {
+		int number = list[index];
+		result += to_string(number) + ", ";
+	}
+	return "[" + result.substr(0, result.length() - 2) + "]";
+}
+
+int main() {
+	string line;
+	while (getline(cin, line)) {
+		vector<int> nums1 = stringToIntegerVector(line);
+		getline(cin, line);
+		int m = stringToInteger(line);
+		getline(cin, line);
+		vector<int> nums2 = stringToIntegerVector(line);
+		getline(cin, line);
+		int n = stringToInteger(line);
+
+		Solution().merge(nums1, m, nums2, n);
+
+		string out = integerVectorToString(nums1);
+		cout << out << endl;
+	}
+	return 0;
+}
+
+//https://leetcode-cn.com/problems/majority-element/submissions/
+
+//class Solution {
+//public:
+//	int majorityElement(vector<int>& nums) {
+//		int target = nums[0];
+//		int count = 1;
+//		for (int i = 1; i < nums.size(); i++)
+//			if (nums[i] != target)
+//			{
+//				count--;
+//				if (count == 0)
+//				{
+//					target = nums[i];
+//					count = 1;
+//				}
+//			}
+//			else count++;
+//		return target;
+//	}
+//};
 
 //https://leetcode-cn.com/problems/sqrtx/
 
