e57bddafc566a69fca2b2043f44852f51fe767ab
Aircraft-King <1069865680@qq.com>
2020-01-10 15:07:45 +0800

    leetcode-学生出勤记录

--- leetcode-牛客/leetcode-牛客/test.cpp
+++ leetcode-牛客/leetcode-牛客/test.cpp
@@ -8,81 +8,102 @@
 #include <algorithm> //sort
 using namespace std;
 
-//https://leetcode-cn.com/problems/find-all-numbers-disappeared-in-an-array/submissions/
+//https://leetcode-cn.com/problems/student-attendance-record-i/submissions/
+
 class Solution {
 public:
-	vector<int> findDisappearedNumbers(vector<int>& nums) {
-		vector<int> v;
-		sort(nums.begin(), nums.end());
-		for (int i = 0; i < nums.size(); ++i) {
-			int j = (nums[i] - 1) % nums.size();
-			nums[j] += nums.size();
-		}
-		for (int i = 0; i < nums.size(); ++i) {
-			if (nums[i] <= nums.size()) {
-				v.push_back(i + 1);
+	bool checkRecord(string s) {
+		int count = 0;
+		for (int i = 0; i < s.size(); ++i) {
+			if (s[i] == 'A') {
+				count++;
+			}
+			if (i < s.size() - 2 && s[i] == 'L' && s[i] == s[i + 1] && s[i] == s[i + 2]) {
+				return false;
 			}
 		}
-		return v;
+		if (count > 1) {
+			return false;
+		}
+		return true;
 	}
 };
 
-void trimLeftTrailingSpaces(string &input) {
-	input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
-		return !isspace(ch);
-	}));
-}
-
-void trimRightTrailingSpaces(string &input) {
-	input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
-		return !isspace(ch);
-	}).base(), input.end());
-}
-
-vector<int> stringToIntegerVector(string input) {
-	vector<int> output;
-	trimLeftTrailingSpaces(input);
-	trimRightTrailingSpaces(input);
-	input = input.substr(1, input.length() - 2);
-	stringstream ss;
-	ss.str(input);
-	string item;
-	char delim = ',';
-	while (getline(ss, item, delim)) {
-		output.push_back(stoi(item));
-	}
-	return output;
-}
-
-string integerVectorToString(vector<int> list, int length = -1) {
-	if (length == -1) {
-		length = list.size();
-	}
-
-	if (length == 0) {
-		return "[]";
-	}
-
-	string result;
-	for (int index = 0; index < length; index++) {
-		int number = list[index];
-		result += to_string(number) + ", ";
-	}
-	return "[" + result.substr(0, result.length() - 2) + "]";
-}
-
-int main() {
-	string line;
-	while (getline(cin, line)) {
-		vector<int> nums = stringToIntegerVector(line);
-
-		vector<int> ret = Solution().findDisappearedNumbers(nums);
-
-		string out = integerVectorToString(ret);
-		cout << out << endl;
-	}
-	return 0;
-}
+//https://leetcode-cn.com/problems/find-all-numbers-disappeared-in-an-array/submissions/
+//class Solution {
+//public:
+//	vector<int> findDisappearedNumbers(vector<int>& nums) {
+//		vector<int> v;
+//		sort(nums.begin(), nums.end());
+//		for (int i = 0; i < nums.size(); ++i) {
+//			int j = (nums[i] - 1) % nums.size();
+//			nums[j] += nums.size();
+//		}
+//		for (int i = 0; i < nums.size(); ++i) {
+//			if (nums[i] <= nums.size()) {
+//				v.push_back(i + 1);
+//			}
+//		}
+//		return v;
+//	}
+//};
+//
+//void trimLeftTrailingSpaces(string &input) {
+//	input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+//		return !isspace(ch);
+//	}));
+//}
+//
+//void trimRightTrailingSpaces(string &input) {
+//	input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+//		return !isspace(ch);
+//	}).base(), input.end());
+//}
+//
+//vector<int> stringToIntegerVector(string input) {
+//	vector<int> output;
+//	trimLeftTrailingSpaces(input);
+//	trimRightTrailingSpaces(input);
+//	input = input.substr(1, input.length() - 2);
+//	stringstream ss;
+//	ss.str(input);
+//	string item;
+//	char delim = ',';
+//	while (getline(ss, item, delim)) {
+//		output.push_back(stoi(item));
+//	}
+//	return output;
+//}
+//
+//string integerVectorToString(vector<int> list, int length = -1) {
+//	if (length == -1) {
+//		length = list.size();
+//	}
+//
+//	if (length == 0) {
+//		return "[]";
+//	}
+//
+//	string result;
+//	for (int index = 0; index < length; index++) {
+//		int number = list[index];
+//		result += to_string(number) + ", ";
+//	}
+//	return "[" + result.substr(0, result.length() - 2) + "]";
+//}
+//
+//int main() {
+//	string line;
+//	while (getline(cin, line)) {
+//		vector<int> nums = stringToIntegerVector(line);
+//
+//		vector<int> ret = Solution().findDisappearedNumbers(nums);
+//
+//		string out = integerVectorToString(ret);
+//		cout << out << endl;
+//	}
+//	return 0;
+//}
 
 //https://leetcode-cn.com/problems/number-of-1-bits/submissions/
 
