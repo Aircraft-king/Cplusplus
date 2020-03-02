b4bbcf8ec28f91ad99ae521890ac0ba326fa746a
Aircraft-King <1069865680@qq.com>
2020-03-02 19:53:55 +0800

    3-2

--- leetcode-牛客/leetcode-牛客/test.cpp
+++ leetcode-牛客/leetcode-牛客/test.cpp
@@ -8,76 +8,98 @@
 #include <algorithm> //sort
 using namespace std;
 
-//https://leetcode-cn.com/problems/remove-all-adjacent-duplicates-in-string/submissions/
-
-class Solution {
-public:
-	string removeDuplicates(string S) {
-		stack<char> s;
-		for (int i = 0; i < S.size(); ++i) {
-			if (s.empty()) s.push(S[i]);
-			else if (s.top() == S[i]) {
-				s.pop();
-			}
-			else {
-				s.push(S[i]);
-			}
-		}
-		
-		stack<char> s1;
-		string str;
-		int len = s.size();
-		for (int i = 0; i < len; ++i) {
-			s1.push(s.top());
-			s.pop();
-		}
-		for (int i = 0; i < len; ++i) {
-			str.push_back(s1.top());
-			s1.pop();
-		}
-		return str;
-	}
-};
-
-string stringToString(string input) {
-	assert(input.length() >= 2);
-	string result;
-	for (int i = 1; i < input.length() - 1; i++) {
-		char currentChar = input[i];
-		if (input[i] == '\\') {
-			char nextChar = input[i + 1];
-			switch (nextChar) {
-			case '\"': result.push_back('\"'); break;
-			case '/': result.push_back('/'); break;
-			case '\\': result.push_back('\\'); break;
-			case 'b': result.push_back('\b'); break;
-			case 'f': result.push_back('\f'); break;
-			case 'r': result.push_back('\r'); break;
-			case 'n': result.push_back('\n'); break;
-			case 't': result.push_back('\t'); break;
-			default: break;
-			}
-			i++;
-		}
-		else {
-			result.push_back(currentChar);
-		}
-	}
-	return result;
-}
-
+//牛客组队竞速
 int main() {
-	string line;
-	while (getline(cin, line)) {
-		string S = stringToString(line);
+	int n = 0;
 
-		string ret = Solution().removeDuplicates(S);
-
-		string out = (ret);
-		cout << out << endl;
+	while (cin >> n) {
+		vector<int> v(3 * n, 0);
+		for (int i = 0; i < 3 * n; ++i) {
+			cin >> v[i];
+		}
+		sort(v.begin(), v.end());
+		long max = 0;
+		int x = 3 * n - 2;
+		while (n--) {
+			max += v[x];
+			x -= 2;
+		}
+		cout << max << endl;
 	}
+
 	return 0;
 }
+
+//https://leetcode-cn.com/problems/remove-all-adjacent-duplicates-in-string/submissions/
+
+//class Solution {
+//public:
+//	string removeDuplicates(string S) {
+//		stack<char> s;
+//		for (int i = 0; i < S.size(); ++i) {
+//			if (s.empty()) s.push(S[i]);
+//			else if (s.top() == S[i]) {
+//				s.pop();
+//			}
+//			else {
+//				s.push(S[i]);
+//			}
+//		}
+//		
+//		stack<char> s1;
+//		string str;
+//		int len = s.size();
+//		for (int i = 0; i < len; ++i) {
+//			s1.push(s.top());
+//			s.pop();
+//		}
+//		for (int i = 0; i < len; ++i) {
+//			str.push_back(s1.top());
+//			s1.pop();
+//		}
+//		return str;
+//	}
+//};
+//
+//string stringToString(string input) {
+//	assert(input.length() >= 2);
+//	string result;
+//	for (int i = 1; i < input.length() - 1; i++) {
+//		char currentChar = input[i];
+//		if (input[i] == '\\') {
+//			char nextChar = input[i + 1];
+//			switch (nextChar) {
+//			case '\"': result.push_back('\"'); break;
+//			case '/': result.push_back('/'); break;
+//			case '\\': result.push_back('\\'); break;
+//			case 'b': result.push_back('\b'); break;
+//			case 'f': result.push_back('\f'); break;
+//			case 'r': result.push_back('\r'); break;
+//			case 'n': result.push_back('\n'); break;
+//			case 't': result.push_back('\t'); break;
+//			default: break;
+//			}
+//			i++;
+//		}
+//		else {
+//			result.push_back(currentChar);
+//		}
+//	}
+//	return result;
+//}
+//
+//int main() {
+//	string line;
+//	while (getline(cin, line)) {
+//		string S = stringToString(line);
+//
+//		string ret = Solution().removeDuplicates(S);
+//
+//		string out = (ret);
+//		cout << out << endl;
+//	}
+//	return 0;
+//}
 
 
 //class Solution {
