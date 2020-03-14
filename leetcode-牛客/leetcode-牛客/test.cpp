b8f585d98cd16496174d07e23e7b388a27a3839c
Aircraft-King <1069865680@qq.com>
2020-03-14 21:12:31 +0800

    3-14 牛客

--- leetcode-牛客/leetcode-牛客/test.cpp
+++ leetcode-牛客/leetcode-牛客/test.cpp
@@ -13,84 +13,106 @@ using namespace std;
 #include<string>
 using namespace std;
 
-int main()
-{
-	string s;
-	s.resize(1000);
-	while (cin >> s)
-	{
-		int n;
-		int f[5] = { 0 };
-		int ans = 0;
-		int num = 0;
-		int letter_small = 0;
-		int capital = 0;
-		int character = 0;
-		n = s.size();
-		for (int i = 0; i < n; i++)
-		{
-			if (s[i] >= '0'&&s[i] <= '9')
-				num++;
-			else if (s[i] >= 'a'&&s[i] <= 'z')
-				letter_small++;
-			else if (s[i] >= 'A'&&s[i] <= 'Z')
-				capital++;
-			else
-				character++;
-		}
-		//ÅĞ¶Ï³¤¶È
-		if (n <= 4)
-			f[0] = 5;
-		else if (n > 4 && n < 8)
-			f[0] = 10;
-		else if (n >= 8)
-			f[0] = 25;
-		//ÅĞ¶Ï×ÖÄ¸
-		if (letter_small + capital == 0)
-			f[1] = 0;
-		else if ((letter_small > 0 && capital == 0) || (letter_small == 0 && capital > 0))
-			f[1] = 10;
-		else if (letter_small > 0 && capital > 0)
-			f[1] = 20;
-		//ÅĞ¶ÏÊı×Ö
-		if (num == 0)
-			f[2] = 0;
-		else if (num == 1)
-			f[2] = 10;
-		else if (num > 1)
-			f[2] = 20;
-		//ÅĞ¶Ï·ûºÅ
-		if (character == 0)
-			f[3] = 0;
-		else if (character == 1)
-			f[3] = 10;
-		else if (character > 1)
-			f[3] = 25;
-		if (letter_small + capital > 0 && num > 0)
-			f[5] = 2;
-		else if (letter_small + capital > 0 && num > 0 && character > 0)
-			f[5] = 3;
-		else if (letter_small > 0 && capital > 0 && num > 0 && character > 0)
-			f[5] = 5;
-		ans = f[0] + f[1] + f[2] + f[3] + f[4];
-		if (ans >= 90)
-			cout << "VERY_SECURE" << endl;
-		else if (ans >= 80)
-			cout << "SECURE" << endl;
-		else if (ans >= 70)
-			cout << "VERY_STRONG" << endl;
-		else if (ans >= 60)
-			cout << "STRONG" << endl;
-		else if (ans >= 50)
-			cout << "AVERAGE" << endl;
-		else if (ans >= 25)
-			cout << "WEAK" << endl;
-		else
-			cout << "VERY_WEAK" << endl;
-	}
-	return 0;
-}
 
+
+//Å£¿Í---×î½ü¹«¹²×æÏÈ
+
+class LCA {
+public:
+	int getLCA(int a, int b) {
+		// write code here
+		while (a&&b) {
+			while (a > b) {
+				a /= 2;
+			}
+			while (b > a) {
+				b /= 2;
+			}
+			if (a == b) {
+				return a;
+			}
+		}
+	}
+};
+
+//int main()
+//{
+//	string s;
+//	s.resize(1000);
+//	while (cin >> s)
+//	{
+//		int n;
+//		int f[5] = { 0 };
+//		int ans = 0;
+//		int num = 0;
+//		int letter_small = 0;
+//		int capital = 0;
+//		int character = 0;
+//		n = s.size();
+//		for (int i = 0; i < n; i++)
+//		{
+//			if (s[i] >= '0'&&s[i] <= '9')
+//				num++;
+//			else if (s[i] >= 'a'&&s[i] <= 'z')
+//				letter_small++;
+//			else if (s[i] >= 'A'&&s[i] <= 'Z')
+//				capital++;
+//			else
+//				character++;
+//		}
+//		//ÅĞ¶Ï³¤¶È
+//		if (n <= 4)
+//			f[0] = 5;
+//		else if (n > 4 && n < 8)
+//			f[0] = 10;
+//		else if (n >= 8)
+//			f[0] = 25;
+//		//ÅĞ¶Ï×ÖÄ¸
+//		if (letter_small + capital == 0)
+//			f[1] = 0;
+//		else if ((letter_small > 0 && capital == 0) || (letter_small == 0 && capital > 0))
+//			f[1] = 10;
+//		else if (letter_small > 0 && capital > 0)
+//			f[1] = 20;
+//		//ÅĞ¶ÏÊı×Ö
+//		if (num == 0)
+//			f[2] = 0;
+//		else if (num == 1)
+//			f[2] = 10;
+//		else if (num > 1)
+//			f[2] = 20;
+//		//ÅĞ¶Ï·ûºÅ
+//		if (character == 0)
+//			f[3] = 0;
+//		else if (character == 1)
+//			f[3] = 10;
+//		else if (character > 1)
+//			f[3] = 25;
+//		if (letter_small + capital > 0 && num > 0)
+//			f[5] = 2;
+//		else if (letter_small + capital > 0 && num > 0 && character > 0)
+//			f[5] = 3;
+//		else if (letter_small > 0 && capital > 0 && num > 0 && character > 0)
+//			f[5] = 5;
+//		ans = f[0] + f[1] + f[2] + f[3] + f[4];
+//		if (ans >= 90)
+//			cout << "VERY_SECURE" << endl;
+//		else if (ans >= 80)
+//			cout << "SECURE" << endl;
+//		else if (ans >= 70)
+//			cout << "VERY_STRONG" << endl;
+//		else if (ans >= 60)
+//			cout << "STRONG" << endl;
+//		else if (ans >= 50)
+//			cout << "AVERAGE" << endl;
+//		else if (ans >= 25)
+//			cout << "WEAK" << endl;
+//		else
+//			cout << "VERY_WEAK" << endl;
+//	}
+//	return 0;
+//}
+//
 
 //Å£¿Í--¾®×ÖÆå
 //class Board {
