58d581d01e1a47ea0222698e64d3f9b052a5d2e8
Aircraft-King <1069865680@qq.com>
2020-03-25 18:26:05 +0800

    牛客--公共子序列长度

--- leetcode-牛客/leetcode-牛客/test.cpp
+++ leetcode-牛客/leetcode-牛客/test.cpp
@@ -8,19 +8,39 @@
 #include <algorithm> //sort
 using namespace std;
 
+//牛客---公共子序列长度
+int main() {
+	string str1, str2;
+	while (cin >> str1 >> str2) {
+		int len1 = str1.size();
+		int len2 = str2.size();
+		int max = 0;
+		vector<vector<int>> dp(len1 + 1, vector<int>(len2 + 1, 0));
+		for (int i = 1; i <= len1; ++i) {
+			for (int j = 1; j <= len2; ++j) {
+				if (tolower(str1[i - 1]) == tolower(str2[j - 1]))
+					dp[i][j] = dp[i - 1][j - 1] + 1;
+				if (dp[i][j] > max)
+					max = dp[i][j];
+			}
+		}
+		cout << max << endl;
+	}
+	return 0;
+}
 
 //牛客--字符串反转
 
-int main() {
-
-	string s;
-	while (getline(cin, s)) {
-		reverse(s.begin(), s.end());
-		cout << s << endl;
-	}
-
-	return 0;
-}
+//int main() {
+//
+//	string s;
+//	while (getline(cin, s)) {
+//		reverse(s.begin(), s.end());
+//		cout << s << endl;
+//	}
+//
+//	return 0;
+//}
 
 
 //牛客--汽水瓶
