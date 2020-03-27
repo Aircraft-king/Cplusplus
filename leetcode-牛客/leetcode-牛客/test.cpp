e6082e04b2b3dab23470ec4afe65507a24eb81f8
Aircraft-King <1069865680@qq.com>
2020-03-27 22:01:21 +0800

    小易的升级之路--3.27

--- leetcode-牛客/leetcode-牛客/test.cpp
+++ leetcode-牛客/leetcode-牛客/test.cpp
@@ -8,26 +8,55 @@
 #include <algorithm> //sort
 using namespace std;
 
-//牛客---公共子序列长度
+//牛客---小易的升级之路
+
+int max_val(int power, int boss) {
+	if (boss == 0)
+		return power;
+	else
+		return max_val(boss, power%boss);
+}
+
 int main() {
-	string str1, str2;
-	while (cin >> str1 >> str2) {
-		int len1 = str1.size();
-		int len2 = str2.size();
-		int max = 0;
-		vector<vector<int>> dp(len1 + 1, vector<int>(len2 + 1, 0));
-		for (int i = 1; i <= len1; ++i) {
-			for (int j = 1; j <= len2; ++j) {
-				if (tolower(str1[i - 1]) == tolower(str2[j - 1]))
-					dp[i][j] = dp[i - 1][j - 1] + 1;
-				if (dp[i][j] > max)
-					max = dp[i][j];
-			}
+	int n;
+	while (cin >> n) {
+		int power;
+		cin >> power;
+		vector<int> v(100000);
+		for (int i = 0; i < n; ++i) {
+			cin >> v[i];
 		}
-		cout << max << endl;
+		for (int i = 0; i < n; ++i) {
+			if (v[i] <= power)
+				power += v[i];
+			else
+				power += max_val(power, v[i]);
+		}
+		cout << power << endl;
 	}
 	return 0;
 }
+
+//牛客---公共子序列长度
+//int main() {
+//	string str1, str2;
+//	while (cin >> str1 >> str2) {
+//		int len1 = str1.size();
+//		int len2 = str2.size();
+//		int max = 0;
+//		vector<vector<int>> dp(len1 + 1, vector<int>(len2 + 1, 0));
+//		for (int i = 1; i <= len1; ++i) {
+//			for (int j = 1; j <= len2; ++j) {
+//				if (tolower(str1[i - 1]) == tolower(str2[j - 1]))
+//					dp[i][j] = dp[i - 1][j - 1] + 1;
+//				if (dp[i][j] > max)
+//					max = dp[i][j];
+//			}
+//		}
+//		cout << max << endl;
+//	}
+//	return 0;
+//}
 
 //牛客--字符串反转
 
