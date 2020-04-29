bfb9f85db3da93786e2c59654d49bed643e93681
Aircraft-King <1069865680@qq.com>
2020-04-29 21:39:15 +0800

    4-29

--- 排序/排序/test.cpp
+++ 排序/排序/test.cpp
@@ -2,16 +2,16 @@
 using namespace std;
 
 
-int main() {
-
-	int arr[10] = { 8,5,7,9,4,1,3,2,6,0 };
-	my_insertsort(arr, sizeof(arr) / sizeof(arr[0]));
-	for (int i = 0; i < 10; i++) {
-		cout << arr[i] << " ";
-	}
-	cout << endl;
-	return 0;
-}
+//int main() {
+//
+//	int arr[10] = { 8,5,7,9,4,1,3,2,6,0 };
+//	my_insertsort(arr, sizeof(arr) / sizeof(arr[0]));
+//	for (int i = 0; i < 10; i++) {
+//		cout << arr[i] << " ";
+//	}
+//	cout << endl;
+//	return 0;
+//}
 
 
 //²åÈëÅÅÐò
@@ -70,39 +70,39 @@ int main() {
 //}
 
 //¿ìÅÅ
-//void my_sort(int *arr,int  low, int high) {
-//	if (low >= high) return;
-//	int i = low;
-//	int j = high+1;
-//	int key = arr[0];
-//	while (low < high) {
-//		while (arr[++i] < key) {
-//			if (i == high)
-//				break;
-//		}
-//		while (arr[--j] > key) {
-//			if (j == low)
-//				break;
-//		}
-//		if (i >= j) break;
-//		int tmp = arr[i];
-//		arr[i] = arr[j];
-//		arr[j] = tmp;
-//	}
-//	int tmp = arr[low];
-//	arr[low] = arr[j];
-//	arr[j] = tmp;
-//	my_sort(arr, low, j - 1);
-//	my_sort(arr, j + 1, high);
-//}
+void my_sort(int *arr,int  low, int high) {
+	if (low >= high) return;
+	int i = low;
+	int j = high+1;
+	int key = arr[low];
+	while (low < high) {
+		while (arr[++i] < key) {
+			if (i == high)
+				break;
+		}
+		while (arr[--j] > key) {
+			if (j == low)
+				break;
+		}
+		if (i >= j) break;
+		int tmp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = tmp;
+	}
+	int tmp = arr[low];
+	arr[low] = arr[j];
+	arr[j] = tmp;
+	my_sort(arr, low, j - 1);
+	my_sort(arr, j + 1, high);
+}
 
-//int main() {
-//
-//	int arr[10] = { 8,5,7,9,4,1,3,2,6,0 };
-//	my_sort(arr,0,9);
-//	for (int i = 0; i < 10; i++) {
-//		cout << arr[i] << " ";
-//	}
-//	cout << endl;
-//	return 0;
-//}
+int main() {
+
+	int arr[10] = { 9,8,7,6,5,4,3,2,1,0};
+	my_sort(arr,0,9);
+	for (int i = 0; i < 10; i++) {
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+	return 0;
+}
