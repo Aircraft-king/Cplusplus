72201c9ccfdb572beb2642ca6e3046f3b23d9ee6
Aircraft-King <1069865680@qq.com>
2020-05-25 21:39:49 +0800

    5-25

--- map和set/map和set/map_set.cpp
+++ map和set/map和set/map_set.cpp
@@ -27,6 +27,26 @@ void test_map() {
 	map<string, string> m;
 	m.insert(pair<string, string>("peach", "桃子"));
 	m.insert(make_pair("banana", "香蕉"));
+	m["apple"] = "苹果";
+	cout << m.size() << endl;
+
+	for (auto &e : m) {
+		cout << e.first << "---" << e.second << endl;
+	}
+	cout << endl;
+	auto ret = m.insert(make_pair("peach", "桃色"));    
+	if (ret.second)        
+		cout << "<peach, 桃色>不在map中, 已经插入" << endl;    
+	else        
+		cout << "键值为peach的元素已经存在：" << ret.first->first << "--->" << ret.first->second << " 插入失败" << endl;
+
+	// 删除key为"apple"的元素    m.erase("apple");
+
+	if (1 == m.count("apple"))        
+		cout << "apple还在" << endl;   
+	else        
+		cout << "apple被吃了" << endl;
+
 }
 int main() {
 	//test_set();
