5defd54bfd02f8d18a1552621df3b805a73b45e4
Aircraft-King <1069865680@qq.com>
2020-05-27 22:06:52 +0800

    5-27

--- map和set/map和set/map_set.cpp
+++ map和set/map和set/map_set.cpp
@@ -5,6 +5,7 @@
 using namespace std;
 
 void test_set() {
+
 	//数组构造set
 	int arr[] = { 1, 3, 5, 7, 9, 2, 4, 6, 8, 0, 1, 3, 5, 7, 9, 2, 4, 6, 8, 0 };
 	set<int> s(arr, arr + sizeof(arr) / sizeof(arr[0]));
