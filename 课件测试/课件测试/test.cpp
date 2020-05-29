fa03887d4bfe1f3aa61c147e3528ee3e1fda820f
Aircraft-King <1069865680@qq.com>
2020-05-29 22:49:40 +0800

    5-29

--- 课件测试/课件测试/test.cpp
+++ 课件测试/课件测试/test.cpp
@@ -1,20 +1,98 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<vector>
 #include <algorithm> 
-//#include<stack>
+#include<stack>
 #include<iostream>
 #include<stdio.h>
 #include<string>
 using namespace std;
 
 int main() {
-	int arr[10] = { 0 };
-	printf("arr = %p\n", arr);
-	printf("&arr= %p\n", &arr);
-	printf("arr+1 = %p\n", arr + 1);
-	printf("&arr+1= %p\n", &arr + 1);
-	return 0;
+	//int a[] = { 1,2,3,4 };
+	//printf("%d\n", sizeof(a));  //16
+	//printf("%d\n", sizeof(a + 0));//4
+	//printf("%d\n", sizeof(*a));//4
+	//printf("%d\n", sizeof(a + 1));//4
+	//printf("%d\n", sizeof(a[1]));//4
+	//printf("%d\n", sizeof(&a));//4
+	//printf("%d\n", sizeof(*&a));//16
+	//printf("%d\n", sizeof(&a + 1));//4
+	//printf("%d\n", sizeof(&a[0]));//4
+	//printf("%d\n", sizeof(&a[0] + 1));//4
+
+	//字符数组 
+	char arr[] = {'a','b','c','d','e','f'}; 
+	printf("%d\n", sizeof(arr)); 
+	printf("%d\n", sizeof(arr+0)); 
+	printf("%d\n", sizeof(*arr)); 
+	printf("%d\n", sizeof(arr[1])); 
+	printf("%d\n", sizeof(&arr)); 
+	printf("%d\n", sizeof(&arr+1)); 
+	printf("%d\n", sizeof(&arr[0]+1)); 
+
+	printf("%d\n", strlen(arr)); 
+	printf("%d\n", strlen(arr + 0)); 
+	printf("%d\n", strlen(*arr)); 
+	printf("%d\n", strlen(arr[1])); 
+	printf("%d\n", strlen(&arr)); 
+	printf("%d\n", strlen(&arr + 1)); 
+	printf("%d\n", strlen(&arr[0] + 1));
+
+	//char arr[] = "abcdef"; 
+	//printf("%d\n", sizeof(arr)); 
+	//printf("%d\n", sizeof(arr + 0)); 
+	//printf("%d\n", sizeof(*arr)); 
+	//printf("%d\n", sizeof(arr[1])); 
+	//printf("%d\n", sizeof(&arr)); 
+	//printf("%d\n", sizeof(&arr + 1)); 
+	//printf("%d\n", sizeof(&arr[0] + 1));
+
+	//printf("%d\n", strlen(arr)); 
+	//printf("%d\n", strlen(arr + 0)); 
+	//printf("%d\n", strlen(*arr)); 
+	//printf("%d\n", strlen(arr[1])); 
+	//printf("%d\n", strlen(&arr)); 
+	//printf("%d\n", strlen(&arr + 1)); 
+	//printf("%d\n", strlen(&arr[0] + 1));
+
+	//char *p = "abcdef"; 
+	//printf("%d\n", sizeof(p)); 
+	//printf("%d\n", sizeof(p + 1)); 
+	//printf("%d\n", sizeof(*p)); 
+	//printf("%d\n", sizeof(p[0])); 
+	//printf("%d\n", sizeof(&p)); 
+	//printf("%d\n", sizeof(&p + 1)); 
+	//printf("%d\n", sizeof(&p[0] + 1));
+
+	//printf("%d\n", strlen(p)); 
+	//printf("%d\n", strlen(p + 1)); 
+	//printf("%d\n", strlen(*p)); 
+	//printf("%d\n", strlen(p[0])); 
+	//printf("%d\n", strlen(&p)); 
+	//printf("%d\n", strlen(&p + 1)); 
+	//printf("%d\n", strlen(&p[0] + 1));
+	//	//二维数组 
+	//int a[3][4] = {0}; 
+	//printf("%d\n",sizeof(a)); 
+	//printf("%d\n",sizeof(a[0][0])); 
+	//printf("%d\n",sizeof(a[0])); 
+	//printf("%d\n",sizeof(a[0]+1)); 
+	//printf("%d\n",sizeof(*(a[0]+1))); 
+	//printf("%d\n",sizeof(a+1)); 
+	//printf("%d\n",sizeof(*(a+1))); 
+	//printf("%d\n",sizeof(&a[0]+1)); 
+	//printf("%d\n",sizeof(*(&a[0]+1))); 
+	//printf("%d\n",sizeof(*a)); 
+	//printf("%d\n",sizeof(a[3]));
 }
+//int main() {
+//	int arr[10] = { 0 };
+//	printf("arr = %p\n", arr);
+//	printf("&arr= %p\n", &arr);
+//	printf("arr+1 = %p\n", arr + 1);
+//	printf("&arr+1= %p\n", &arr + 1);
+//	return 0;
+//}
 
 //int main() {
 //	int arr[10] = { 0 };
