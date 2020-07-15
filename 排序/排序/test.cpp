281e4af0eec1c807e44c4853cf91ea0cfc5b7987
Aircraft-King <1069865680@qq.com>
2020-07-15 22:50:14 +0800

    7-15

--- 排序/排序/test.cpp
+++ 排序/排序/test.cpp
@@ -70,39 +70,81 @@ using namespace std;
 //}
 
 //¿ìÅÅ
-void my_sort(int *arr,int  low, int high) {
-	if (low >= high) return;
-	int i = low;
-	int j = high+1;
-	int key = arr[low];
-	while (low < high) {
-		while (arr[++i] < key) {
-			if (i == high)
-				break;
+//void my_sort(int *arr,int  low, int high) {
+//	if (low >= high) return;
+//	int i = low;
+//	int j = high+1;
+//	int key = arr[low];
+//	while (low < high) {
+//		while (arr[++i] < key) {
+//			if (i == high)
+//				break;
+//		}
+//		while (arr[--j] > key) {
+//			if (j == low)
+//				break;
+//		}
+//		if (i >= j) break;
+//		int tmp = arr[i];
+//		arr[i] = arr[j];
+//		arr[j] = tmp;
+//	}
+//	int tmp = arr[low];
+//	arr[low] = arr[j];
+//	arr[j] = tmp;
+//	my_sort(arr, low, j - 1);
+//	my_sort(arr, j + 1, high);
+//}
+//
+//int main() {
+//
+//	int arr[10] = { 9,8,7,6,5,4,3,2,1,0};
+//	my_sort(arr,0,9);
+//	for (int i = 0; i < 10; i++) {
+//		cout << arr[i] << " ";
+//	}
+//	cout << endl;
+//	return 0;
+//}
+
+void quicksort(int* arr, int first, int last)
+{
+	if (first >= last)
+		return;
+	int low = first;
+	int high = last;
+	int tmp = arr[first];
+	
+	while (low < high)
+	{
+		while (arr[high] > tmp&&low < high)
+		{
+			high--;
 		}
-		while (arr[--j] > key) {
-			if (j == low)
-				break;
+		if (arr[high] < arr[low])
+		{
+			arr[low] = arr[high];
 		}
-		if (i >= j) break;
-		int tmp = arr[i];
-		arr[i] = arr[j];
-		arr[j] = tmp;
+		while (arr[low] < tmp&& low < high)
+		{
+			low++;
+		}
+		if (arr[low] > arr[high])
+		{
+			arr[high] = arr[low];;
+		}
 	}
-	int tmp = arr[low];
-	arr[low] = arr[j];
-	arr[j] = tmp;
-	my_sort(arr, low, j - 1);
-	my_sort(arr, j + 1, high);
+	arr[low] = tmp;
+	quicksort(arr, first, low - 1);
+	quicksort(arr, low + 1, last);
 }
-
-int main() {
-
-	int arr[10] = { 9,8,7,6,5,4,3,2,1,0};
-	my_sort(arr,0,9);
-	for (int i = 0; i < 10; i++) {
-		cout << arr[i] << " ";
+int main()
+{
+	int arr[10] = { 9,8,7,6,5,4,3,2,1,0 };
+	quicksort(arr, 0, sizeof(arr) / sizeof(arr[0])-1);
+	for (int i = 0; i < 10; i++)
+	{
+		cout << arr[i]<<" ";
 	}
-	cout << endl;
 	return 0;
 }
